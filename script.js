0b967f5c4b9a7fffd7f09cb427781960801f4df9
Glitch (glitch-blank-website) <none>
2022-02-08 10:20:46 +0000

    ⛷📈 Checkpoint ./index.html:27869882/1126 ./style.css:27869882/815 ./script.js:27869882/592

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-console.log("Hello, world!");
+function loadpage(){
+  fetch(
+    `https://api.sheetson.com/v2/sheets/Masks`,
+    {
+      headers: {
+        Authorization:
+          "Bearer Wga4ggDAp0JkrhjcppkjovcjtzsCOj-CSDROrRExAQkD193br8wE0bwEKts",
+        "X-Spreadsheet-Id": "1-ZB401_2pa2zhs5iFFee65_Hgv1VqKJMsyoZUiORtts"
+      }
+    }
+  )
+    .then(r => r.json())
+    .then(result => cons(result.results));
+}
+function cons(e){
+    console.log(e.sort((a, b) => new Date(a.EXP_Date) - new Date(b.EXP_Date)))
+}
